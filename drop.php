576adaf3ae4d89c6ba67539a0f89e0f2ae8f5fa2
MarcGalvez1 <marc.galvez83@gmail.com>
2023-12-03 00:37:01 -0500

    Created the drop script

--- drop.php
+++ drop.php
@@ -11,13 +11,43 @@
     -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A</title>
+    <title>Drop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <?php require("library.php") ?>
+    <?php require("dbconnect.php") ?>
 </head>
 
 <body class="bg-dark">
-
+    <div class="card mx-auto mt-5" style="width:25%" id="visit-container">
+        <?php
+        if (isset($_SESSION["tbName"])) {
+            $dropQuery = "DROP TABLE " . $_SESSION['tbName'];
+            if ($conn->query($dropQuery) === TRUE) {
+        ?>
+                <h5 class="card-header text-center bg-info">The <?php $_SESSION["tbName"]; ?> table has been dropped, please create a new table in the create page</h5>
+            <?php
+            } else {
+            ?>
+                <h5 class="card-header text-center bg-info">Error table could not be deleted. <?php $conn->error; ?></h5>
+            <?php
+            }
+            session_unset();
+            session_destroy();
+        } else {
+            ?>
+            <h5 class="card-header text-center bg-info">There is no table to delete, please create a new table.</h5>
+        <?php
+        }
+        ?>
+        <div class="card-body">
+            <?php
+            PutButton("create", "Create the table");
+            PutButton("insert", "Insert default data into the table");
+            PutButton("show", "Show data inside the table");
+            PutButton("drop", "Drop the table");
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
