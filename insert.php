a57ec76048dfe8709cf5e27b20f78313eb94b167
MarcGalvez1 <marc.galvez83@gmail.com>
2023-12-02 21:03:00 -0500

    Made the initial display of the data

--- insert.php
+++ insert.php
@@ -20,18 +20,35 @@
 <body class="bg-dark">
     <div class="card mx-auto mt-5" style="width:25%">
         <?php
-        $newUsername = "Mr. Smith";
-        $newPassword = "noword";
-        // insert Mr. Smith's data into the user table
-        $checkQuery = "SELECT * FROM " . $_SESSION["tbName"] . " WHERE username = '$newUsername'";
-        $checkResult = $conn->query($checkQuery);
-        $insertQuery = "INSERT INTO " . $_SESSION["tbName"] . " (username, pword)
-            VALUES ('Mr. Smith', 'noword')";
-        if ($checkResult->num_rows == 0) {
-            $insertQuery = "INSERT INTO " . $_SESSION["tbName"] . " (username, pword)
-        VALUES ('$newUsername', '$newPassword')";
-            if ($conn->query($insertQuery) === TRUE) {
+        if (isset($_SESSION["tbName"])) {
+            $newUsername = "Mr. Smith";
+            $newPassword = "noword";
+            // insert Mr. Smith's data into the user table
+            $checkQuery = "SELECT * FROM " . $_SESSION["tbName"] . " WHERE username = '$newUsername'";
+            $checkResult = $conn->query($checkQuery);
+            if ($checkResult->num_rows == 0) {
+                $insertQuery = "INSERT INTO " . $_SESSION["tbName"] . " (username, pword)
+                VALUES ('$newUsername', '$newPassword')";
+                if ($conn->query($insertQuery) === TRUE) {
         ?>
+                    <h5 class="card-header text-center bg-info">The default data has been inserted, Please go to these pages for any other actions.</h5>
+                    <div class="card-body">
+                        <?php
+                        PutButton("create", "Create new table");
+                        PutButton("show", "Show data inside the table");
+                        PutButton("clear", "Clear the table");
+                        PutButton("drop", "Drop the table");
+                        ?>
+                    </div>
+                <?php
+                } else {
+                ?>
+                    <h5 class="card-header text-center bg-info">Unable to insert the data into <?php $_SESSION["tbName"] ?> </h5>
+                    <p class="card-body">Unable to connect to the data base please come back later.</p>
+                <?php
+                }
+            } else {
+                ?>
                 <h5 class="card-header text-center bg-info">The default data has been inserted, Please go to these pages for any other actions.</h5>
                 <div class="card-body">
                     <?php
@@ -41,27 +58,12 @@
                     PutButton("drop", "Drop the table");
                     ?>
                 </div>
-            <?php
-            } else {
-            ?>
-                <h5 class="card-header text-center bg-info">Unable to insert the data into <?php $_SESSION["tbName"] ?> </h5>
-                <p class="card-body">Unable to connect to the data base please come back later.</p>
-            <?php
+        <?php
             }
         } else {
-            ?>
-            <h5 class="card-header text-center bg-info">The default data has been inserted, Please go to these pages for any other actions.</h5>
-            <div class="card-body">
-                <?php
-                PutButton("create", "Create new table");
-                PutButton("show", "Show data inside the table");
-                PutButton("clear", "Clear the table");
-                PutButton("drop", "Drop the table");
-                ?>
-            </div>
-        <?php
+            WinAlert("Please register yourself before doing any further actions, you will now be redirected to the create page.");
+            redirect("create.php");
         }
-
         ?>
     </div>
 </body>
--- show.php
+++ show.php
@@ -11,13 +11,31 @@
     -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A</title>
+    <title>Show Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <?php require("library.php") ?>
+    <?php require("dbconnect.php") ?>
 </head>
 
-<body class="bg-dark">
+<body class="">
+    <?php if (isset($_SESSION["tbName"])) { ?>
+        <div class="card mx-auto mt-5" style="width:25%">
+            <h5 class="card-header text-center bg-info">Showing the data in <?php echo $_SESSION["tbName"]; ?> table.</h5>
+            <?php
+            $showQuery = "SELECT username, pword, creation FROM " . $_SESSION['tbName'];
+            $result = $conn->query($showQuery);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while ($row = $result->fetch_assoc()) {
+                    echo  "username: " . $row["username"] . " " . $row["pword"] . " " . $row["creation"] . "<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            ?>
 
+        </div>
+    <?php } ?>
 </body>
 
 </html>
