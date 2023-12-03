06c43b28a3ed4d0b156b98f92079e3f022d9351d
MarcGalvez1 <marc.galvez83@gmail.com>
2023-12-03 00:54:57 -0500

    added the nav bar in all pages

--- clear.php
+++ clear.php
@@ -18,6 +18,29 @@
 </head>
 
 <body class="bg-dark">
+    <nav class="navbar navbar-expand navbar-dark bg-primary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto me-auto mb-2 mb-lg">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="create.php" title="Create the table">Create</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="insert.php" title="Insert default data into the table">Insert</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="show.php" title="Show data inside the table">Show</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="clear.php" title="clear table">Clear</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="drop.php" title="Drop the table">Drop</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="card mx-auto mt-5" style="width:25%">
 
         <?php
